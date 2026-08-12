79d53333e3ca3d9103316614c40f4cf9db0b9a6c
afomi <afomitlahun@gmail.com>
2026-08-12 06:50:23 +0300

    fanil vidio 5

--- component/focusTime.js
+++ component/focusTime.js
@@ -1,16 +1,16 @@
 import {View,Text,StyleSheet,TouchableOpacity} from 'react-native'
 import { SafeAreaView} from 'react-native-safe-area-context'
-import {useState,useEffect} from 'react'
+import {useState,useEffect} from 'react'//useEffect=and nger sikeyer weym compont sikefet code masked
 
 export default function FocusTime({focusTask,onBack}){
     const times = [600,900, 1200];
-    const [isRunning,setIsRunning ] = useState(false);
-    const[selectedTime,setSelectedTime] = useState(null)
+    const [isRunning,setIsRunning ] = useState(false);//ahun timeru eyesera new wey yemilewn yyzal(false)yehonebet mejemeriya timeru ayseram
+    const[selectedTime,setSelectedTime] = useState(null)//yetgnawn time mertku
     const timeFormat = (time) =>{
       const minutes = Math.floor(time/60);
       const second =Math.floor(time%60);
 
-      return `${minutes}:${second<10 ? '0':''}${second}`;
+      return `${minutes}:${second<10 ? '0':''}${second}`;//5minut:3second =5:03
     }
 
     return(
@@ -20,23 +20,28 @@ export default function FocusTime({focusTask,onBack}){
            < Text style={styles.subTite}>focusing on : </Text>
            <Text style={styles.focusTask}> {focusTask}</Text>
 
-          <View style={styles.selectedText}/>
-
+          <View style={styles.underselectedText}/>
+          <Text style={styles.styleTask}> Task  </Text>
           <View style={styles.timeOptions}>
             {times.map((time,index) =>( 
-              <TouchableOpacity key={index} style={styles.timeOptionsButton} onPress={()=> {}}>
+              <TouchableOpacity 
+              key={index} 
+                
+              
+              style={styles.timeOptionsButton} 
+              onPress={()=> {}}>
+
                 <Text style={styles.timeOptionText}> {timeFormat(time)}</Text>
               </TouchableOpacity>
             ))}
-            <Text> Task  </Text>
           </View>
 
-         <TouchableOpacity>
-            <Text>Start</Text>
+         <TouchableOpacity style={styles.startFab} onPress={()=>{}}>
+            <Text style={{color:'white'}}>{isRunning?'stop':'start'}</Text>
          </TouchableOpacity>
 
-          <TouchableOpacity>
-            <Text>Back</Text>
+          <TouchableOpacity style={styles.backFab} onPress={onBack}>
+            <Text style={{color:'#fff'}}>Back</Text>
          </TouchableOpacity>
 
         </SafeAreaView>
@@ -46,6 +51,7 @@ const styles= StyleSheet.create({
 continer:{
   flex:1,
   backgroundColor:'#0d91a0',
+
   alignItems:'center',
 
 },
@@ -67,10 +73,10 @@ focusTask:{
   fonitSize:30,
   color:'#fff',
   marginTope:50,
-  fontWeight:'bold',
+ fontWeight:'bold',
 
 },
-selectedText:{ 
+underselectedText:{ 
   height:10,
   width:'100%',
   backgroundColor:'#6ccdd7',
@@ -80,14 +86,57 @@ selectedText:{
 },
 timeOptions:{
   flexDirection:'row',
+  marginTop:30,
+  alignItime:'center',
+  //width:'100%',
+  //justifyContent:'space-around',
 },
 timeOptionsButton:{
-height:60,
-width:60,
-borderRadius:30,
+height:70,
+width:70,
+borderRadius:35,
 justifyContent:'center',
 alignItems:'center',
 backgroundColor:'transparent',
+borderWidth:2,
+borderColor:'#fff',
+margin:25,
+},
+timeOptionText:{
+  fontSize:18,
+  color:'#fff',
+},
+startStyle:{
+  margintop:50,
+},
+startFab:{
+  height:80,
+  width:80,
+  borderRadius:40,
+  backgroundColor:'transparent',
+  borderColor:'#fff',
+  justifyContent:'center',
+  alignItems:'center',
+  marginTop:50,
+  borderWidth:2,
+},
+backFab:{
+  height:30,
+  width:80,
+  borderColor:'#fff',
+  borderRadius:20,
+  borderWidth:2,
+  marginTop:50,
+  alignItems:'center',
+},
+styleTask:{
+  alignItems:'center',
+  color:'#fff',
+  fontSize:15,
+  textAlign:'center',
+ // alignSelf:'center',
+  marginBottom:20,
+
 }
 })
 
